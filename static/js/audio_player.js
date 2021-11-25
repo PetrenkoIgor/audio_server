11e0e698c793054b8b369d3da01c1f199d810031
Piter <igor.rock.rayt@gmial.com>
2021-11-25 13:18:20 +0200

    update to last version js

--- static/js/audio_player.js
+++ static/js/audio_player.js
@@ -1,7 +1,10 @@
-var url = "http://0.0.0.0:7006/?cb=" + new Date().getTime();
+var currentLocation = window.location.hostname;
+console.log(currentLocation)
+var url = "http://"+ currentLocation + ":7006/?cb=" + new Date().getTime();
 var audio = document.getElementById('audio_content');
 audio.src = url;
-audio.addEventListener("waiting", event => {
+console.log(url.status)
+audio.addEventListener("warnings", event => {
     console.log('wait');
 });
 audio.onwaiting=function(){
@@ -19,3 +22,7 @@ audio.addEventListener("ended", event => {
         console.log('wait');
     });
 });
+audio.addEventListener("error", event=>{
+    console.log("something",event);
+//    location.reload();
+})
